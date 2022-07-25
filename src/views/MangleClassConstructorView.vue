249627c9dbbee042cf47a08fdd1e3183314be000
euberdeveloper <euberdeveloper@gmail.com>
2022-07-25 12:47:53 +0200

    feat: add mangle pages
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/views/MangleClassConstructorView.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-fade-transition hide-on-leave>
+    <v-main :key="$route.fullPath">
+      <v-container fluid fill-height>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="10">
+            <mangle-class-constructor  />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-fade-transition>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import MangleClassConstructor from "@/components/pages/MangleClassConstructor.vue";
+
+@Component({
+  components: {
+    MangleClassConstructor,
+  },
+})
+export default class MangleClassConstructorView extends Vue {}
+</script>
