160482c15d3fad6eaf4e3663be2ea37dcbef516e
euberdeveloper <euberdeveloper@gmail.com>
2022-07-25 12:07:58 +0200

    feat: add vuetify and skeleton
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="12" md="10">
+        <v-card class="tile">
+          <v-card-title>Titolo</v-card-title>
+          <v-card-text>Ciao {{mangledClass}}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue  } from "vue-property-decorator";
+import { mangleClassName } from 'lifeware-java-mangler';
+
+
+@Component({
+  components: {
+  },
+})
+export default class Home extends Vue {
+  /* DATA */
+  private class = 'java.lang.String';
+
+  /* GETTERS */
+
+  get mangledClass() {
+    return mangleClassName(this.class);
+  }
+}
+</script>
