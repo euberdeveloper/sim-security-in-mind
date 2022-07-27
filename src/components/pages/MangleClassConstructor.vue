f4233f598e21cca4039e051544edb8f0b79fd2b9
euberdeveloper <euberdeveloper@gmail.com>
2022-07-27 10:45:43 +0200

    feat: add validation to constructor and methods
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/pages/MangleClassConstructor.vue
+++ src/components/pages/MangleClassConstructor.vue
@@ -1,34 +1,36 @@
 <template>
   <section-card title="Mangle class constructor">
     <v-card-text class="mt-4 mx-2">
-      <v-text-field v-model="classConstructorSignature" type="text" label="Class constructor" outlined dense />
+      <v-text-field v-model="classConstructorSignature" type="text" label="Class constructor" outlined dense :rules="[validationRule]" />
       <v-text-field
-        :value="mangledClassSignature"
+        :value="mangledClassSignatureShown"
         type="text"
         label="Mangled signature"
         outlined
         dense
         readonly
         append-icon="mdi-content-copy"
-        @click:append="copyText(mangledClassSignature)"
+        @click:append="copyText(mangledClassSignatureShown)"
+        v-if="mangledClassSignatureShown"
       />
       <v-textarea
-        :value="mangledClassDefinition"
+        :value="mangledClassDefinitionShown"
         type="text"
         label="Mangled definition"
         outlined
         dense
         readonly
         append-icon="mdi-content-copy"
-        @click:append="copyText(mangledClassDefinition)"
+        @click:append="copyText(mangledClassDefinitionShown)"
+        v-if="mangledClassDefinitionShown"
       />
     </v-card-text>
   </section-card>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { mangleConstructorDefinition, mangleConstructorSignature } from "lifeware-java-mangler";
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { mangleConstructorDefinition, mangleConstructorSignature, validateConstructorSignature } from "lifeware-java-mangler";
 import clipboardCopy from "clipboard-copy";
 
 import SectionCard from "@/components/gears/SectionCard.vue";
@@ -42,29 +44,55 @@ export default class MangleClassConstructor extends Vue {
   /* DATA */
 
   private classConstructorSignature = "";
+  private fallbackMangledClassConstructorSignature: string | null = null;
+  private fallbackMangledClassConstructorDefinition: string | null = null;
 
   /* GETTERS */
 
-  get mangledClassSignature(): string {
-    try {
-      return mangleConstructorSignature(this.classConstructorSignature);
-    } catch (error) {
-      return "";
-    }
+  get validClassConstructorSignature(): boolean {
+    return validateConstructorSignature(this.classConstructorSignature);
   }
 
-  get mangledClassDefinition(): string {
-    try {
-      return mangleConstructorDefinition(this.classConstructorSignature);
-    } catch (error) {
-      return "";
-    }
+  get mangledClassConstructorSignature(): string | null {
+    return this.validClassConstructorSignature ? mangleConstructorSignature(this.classConstructorSignature) : null;
+  }
+
+  get mangledClassConstructorDefinition(): string | null {
+    return this.validClassConstructorSignature ? mangleConstructorDefinition(this.classConstructorSignature) : null;
+  }
+
+  get mangledClassSignatureShown(): string | null {
+    return this.mangledClassConstructorSignature ?? this.fallbackMangledClassConstructorSignature;
+  }
+
+  get mangledClassDefinitionShown(): string | null {
+    return this.mangledClassConstructorDefinition ?? this.fallbackMangledClassConstructorDefinition;
   }
 
   /* METHODS */
 
   copyText(text: string): void {
     clipboardCopy(text);
+  }
+
+  validationRule() {
+    return this.validClassConstructorSignature;
+  }
+
+  /* WATCHERS */
+
+  @Watch("mangledClassConstructorSignature")
+  watchMangledConstructorSignature(): void {
+    if (this.mangledClassConstructorSignature) {
+      this.fallbackMangledClassConstructorSignature = this.mangledClassConstructorSignature;
+    }
+  }
+
+  @Watch("mangledClassConstructorDefinition")
+  watchMangledConstructorDefinition(): void {
+    if (this.mangledClassConstructorDefinition) {
+      this.fallbackMangledClassConstructorDefinition = this.mangledClassConstructorDefinition;
+    }
   }
 }
 </script>
--- src/components/pages/MangleClassMethod.vue
+++ src/components/pages/MangleClassMethod.vue
@@ -1,34 +1,36 @@
 <template>
   <section-card title="Mangle class method">
     <v-card-text class="mt-4 mx-2">
-      <v-text-field v-model="classMethodSignature" type="text" label="Class method" outlined dense />
+      <v-text-field v-model="classMethodSignature" type="text" label="Class method" outlined dense :rules="[validationRule]" />
       <v-text-field
-        :value="mangledClassMethodSignature"
+        :value="mangledClassSignatureShown"
         type="text"
         label="Mangled signature"
         outlined
         dense
         readonly
         append-icon="mdi-content-copy"
-        @click:append="copyText(mangledClassMethodSignature)"
+        @click:append="copyText(mangledClassSignatureShown)"
+        v-if="mangledClassSignatureShown"
       />
       <v-textarea
-        :value="mangledClassMethodDefinition"
+        :value="mangledClassDefinitionShown"
         type="text"
         label="Mangled definition"
         outlined
         dense
         readonly
         append-icon="mdi-content-copy"
-        @click:append="copyText(mangledClassMethodDefinition)"
+        @click:append="copyText(mangledClassDefinitionShown)"
+        v-if="mangledClassDefinitionShown"
       />
     </v-card-text>
   </section-card>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { mangleMethodSignature, mangleMethodDefinition } from "lifeware-java-mangler";
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { mangleMethodDefinition, mangleMethodSignature, validateMethodSignature } from "lifeware-java-mangler";
 import clipboardCopy from "clipboard-copy";
 
 import SectionCard from "@/components/gears/SectionCard.vue";
@@ -42,29 +44,55 @@ export default class MangleClassMethod extends Vue {
   /* DATA */
 
   private classMethodSignature = "";
+  private fallbackMangledClassMethodSignature: string | null = null;
+  private fallbackMangledClassMethodDefinition: string | null = null;
 
   /* GETTERS */
 
-  get mangledClassMethodSignature(): string {
-    try {
-      return mangleMethodSignature(this.classMethodSignature);
-    } catch (error) {
-      return "";
-    }
+  get validClassMethodSignature(): boolean {
+    return validateMethodSignature(this.classMethodSignature);
   }
 
-  get mangledClassMethodDefinition(): string {
-    try {
-      return mangleMethodDefinition(this.classMethodSignature);
-    } catch (error) {
-      return "";
-    }
+  get mangledClassMethodSignature(): string | null {
+    return this.validClassMethodSignature ? mangleMethodSignature(this.classMethodSignature) : null;
+  }
+
+  get mangledClassMethodDefinition(): string | null {
+    return this.validClassMethodSignature ? mangleMethodDefinition(this.classMethodSignature) : null;
+  }
+
+  get mangledClassSignatureShown(): string | null {
+    return this.mangledClassMethodSignature ?? this.fallbackMangledClassMethodSignature;
+  }
+
+  get mangledClassDefinitionShown(): string | null {
+    return this.mangledClassMethodDefinition ?? this.fallbackMangledClassMethodDefinition;
   }
 
   /* METHODS */
 
   copyText(text: string): void {
     clipboardCopy(text);
+  }
+
+  validationRule() {
+    return this.validClassMethodSignature;
+  }
+
+  /* WATCHERS */
+
+  @Watch("mangledClassMethodSignature")
+  watchMangledMethodSignature(): void {
+    if (this.mangledClassMethodSignature) {
+      this.fallbackMangledClassMethodSignature = this.mangledClassMethodSignature;
+    }
+  }
+
+  @Watch("mangledClassMethodDefinition")
+  watchMangledMethodDefinition(): void {
+    if (this.mangledClassMethodDefinition) {
+      this.fallbackMangledClassMethodDefinition = this.mangledClassMethodDefinition;
+    }
   }
 }
 </script>
