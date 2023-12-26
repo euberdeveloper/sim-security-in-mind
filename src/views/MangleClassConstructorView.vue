f1f72ce1d913c388207c83fccf4ef01608b9d959
euberdeveloper <euberdeveloper@gmail.com>
2023-12-26 01:19:58 +0100

    feat: add class constructor
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/views/MangleClassConstructorView.vue
+++ src/views/MangleClassConstructorView.vue
@@ -1,5 +1,51 @@
+<script setup lang="ts">
+import { mangleConstructorDefinition, mangleConstructorSignature, validateConstructorSignature } from "lifeware-java-mangler";
+import { storeToRefs } from 'pinia';
+
+import { useClipboard } from '@/compositions/clipboard';
+import { useMangling, useManglingValidation } from '@/compositions/mangler';
+import { useManglersStore } from '@/stores/manglers';
+
+const { copyToClipboard } = useClipboard();
+const { classConstructor } = storeToRefs(useManglersStore());
+const { isInputValid, mangledValidationRule } = useManglingValidation(classConstructor, validateConstructorSignature);
+const { displayedMangled: displayedMangledConstructorSignature } = useMangling(classConstructor, isInputValid, mangleConstructorSignature);
+const { displayedMangled: displayedMangledConstructorDefinition } = useMangling(classConstructor, isInputValid, mangleConstructorDefinition);
+</script>
+
 <template>
-  <div>
-    <h1>Mangle Class Constructor View</h1>
-  </div>
+  <v-card-text class="mt-4 mx-2">
+    <v-text-field
+      v-model="classConstructor"
+      type="text"
+      label="Class constructor"
+      variant="outlined"
+      density="compact"
+      :rules="[mangledValidationRule]"
+    />
+    <v-text-field
+      :value="displayedMangledConstructorSignature"
+      type="text"
+      label="Mangled signature"
+      variant="outlined"
+      density="compact"
+      readonly
+      persistent-placeholder
+      append-inner-icon="mdi-content-copy"
+      @click:append-inner="copyToClipboard(displayedMangledConstructorSignature)"
+      v-if="displayedMangledConstructorSignature"
+    />
+    <v-textarea
+      :value="displayedMangledConstructorDefinition"
+      type="text"
+      label="Mangled definition"
+      variant="outlined"
+      density="compact"
+      readonly
+      persistent-placeholder
+      append-inner-icon="mdi-content-copy"
+      @click:append-inner="copyToClipboard(displayedMangledConstructorDefinition)"
+      v-if="displayedMangledConstructorDefinition"
+    />
+  </v-card-text>
 </template>
