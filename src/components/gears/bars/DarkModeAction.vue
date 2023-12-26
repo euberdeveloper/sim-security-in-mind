f3aa3c1a91a4f7b7089a180de974623e52c62e5b
euberdeveloper <euberdeveloper@gmail.com>
2023-12-26 17:31:31 +0100

    feat(theme): make primary color finally work
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/gears/bars/DarkModeAction.vue
+++ src/components/gears/bars/DarkModeAction.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { useThemeStore } from '@/stores/theme';
-const { toggleTheme } = useThemeStore();
+const { toggleDarkMode } = useThemeStore();
 </script>
 
 <template>
     <v-tooltip text="Modalità notte" location="bottom">
         <template v-slot:activator="{ props }">
-            <v-btn v-bind="props" icon="mdi-theme-light-dark" @click="toggleTheme" />
+            <v-btn v-bind="props" icon="mdi-theme-light-dark" @click="toggleDarkMode" />
         </template>
     </v-tooltip>
 </template>
--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,6 +1,6 @@
 import { computed, ref, watchEffect, watch } from 'vue';
 import { defineStore, storeToRefs } from 'pinia';
-import { useTheme } from 'vuetify';
+import { useTheme, type ThemeDefinition } from 'vuetify';
 import {
   VFadeTransition,
   VScaleTransition,
@@ -9,24 +9,51 @@ import {
   VSlideYTransition
 } from 'vuetify/components';
 
+export interface VuetifyThemeBaseColors {
+    background: string;
+    surface: string;
+    primary: string;
+    secondary: string;
+    success: string;
+    warning: string;
+    error: string;
+    info: string;
+}
+export interface VuetifyThemeOnColors {
+    'on-background': string;
+    'on-surface': string;
+    'on-primary': string;
+    'on-secondary': string;
+    'on-success': string;
+    'on-warning': string;
+    'on-error': string;
+    'on-info': string;
+}
+export interface VuetifyThemeColors extends VuetifyThemeBaseColors, VuetifyThemeOnColors {
+  [key: string]: string;
+}
+
 export const useThemeStore = defineStore('theme', () => {
   const theme = useTheme();
 
   const isDark = ref(theme.current.value.dark);
-  const themeColors = ref({ ...theme.current.value.colors });
   const currentThemeName = computed(() => (isDark.value ? 'dark' : 'light'));
+  const themes = ref<Record<string, ThemeDefinition>>({ ...theme.themes.value });
+  const themeColors = computed<VuetifyThemeColors>(() => themes.value[currentThemeName.value].colors as VuetifyThemeColors);
   const primaryColor = computed({
     get: () => themeColors.value.primary,
     set: (value) => (themeColors.value.primary = value)
   });
-  function applyTheme() {
-    theme.global.name.value = currentThemeName.value;
-    theme.themes.value[currentThemeName.value].colors = themeColors.value;
-  }
   function toggleDarkMode() {
     isDark.value = !isDark.value;
   }
-  const darkComponents = { isDark, currentThemeName, themeColors, primaryColor, toggleDarkMode, applyTheme };
+  function applyThemeDarkMode() {
+    theme.global.name.value = currentThemeName.value;
+  }
+  function applyThemeColors() {
+    theme.themes.value[currentThemeName.value].colors = themeColors.value;
+  }
+  const paletteComponents = { isDark, currentThemeName, themes, themeColors, primaryColor, toggleDarkMode, applyThemeDarkMode, applyThemeColors};
 
   const transitionMapping = {
     VFadeTransition,
@@ -49,23 +76,26 @@ export const useThemeStore = defineStore('theme', () => {
   }
   const transitionComponents = { routeTransition, transitionNames, setRouteTransition };
 
-  return { ...darkComponents, ...transitionComponents };
+  return { ...paletteComponents, ...transitionComponents };
 });
 
 export function syncThemeStoreWithLocalStorage(localStorageKey: string) {
   const themeStore = useThemeStore();
-  const { isDark, themeColors } = storeToRefs(themeStore);
+  const { isDark, currentThemeName, themeColors } = storeToRefs(themeStore);
   const localTheme = localStorage.getItem(localStorageKey);
 
   if (localTheme) {
     themeStore.$patch(JSON.parse(localTheme));
-    themeStore.applyTheme();
+    themeStore.applyThemeDarkMode();
   }
 
   watchEffect(() => {
     localStorage.setItem(localStorageKey, JSON.stringify(themeStore.$state));
   });
-  watch([isDark, themeColors], () => {
-    themeStore.applyTheme();
+  watch([isDark, currentThemeName], () => {
+    themeStore.applyThemeDarkMode();
+  });
+  watch(themeColors, () => {
+    themeStore.applyThemeColors();
   });
 }
