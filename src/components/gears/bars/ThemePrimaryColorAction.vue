bd6e6886451965538afae71c25a35a800a90d0fc
euberdeveloper <euberdeveloper@gmail.com>
2023-12-26 17:43:27 +0100

    feat(bar): remove random primary color for mobile
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/gears/bars/ThemePrimaryColorAction.vue
+++ src/components/gears/bars/ThemePrimaryColorAction.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref , computed} from 'vue';
 import { storeToRefs } from 'pinia';
+import {useDisplay} from 'vuetify';
 
 import { useThemeStore } from '@/stores/theme';
 
@@ -8,14 +9,17 @@ const themeStore = useThemeStore();
 const {primaryColor} = storeToRefs(themeStore);
 const { randomPrimaryColor } = themeStore;
 
+const display = useDisplay();
+const isMobile = computed(() => display.mobile.value );
+
 const paletteMenuOpen = ref(false);
 </script>
 
 <template>
-  <v-menu v-model="paletteMenuOpen" location="start bottom" open-on-hover :close-on-content-click="false"
+  <v-menu v-model="paletteMenuOpen" location="start bottom" :open-on-hover="!isMobile" :close-on-content-click="false"
     class="rounded-pill">
     <template v-slot:activator="{ props }">
-      <v-btn v-bind="{ ...props, }" icon="mdi-format-color-fill" @click="randomPrimaryColor" />
+      <v-btn v-bind="{ ...props, }" icon="mdi-format-color-fill" @click="!isMobile && randomPrimaryColor()" />
     </template>
     <v-color-picker class="mt-4" style="overflow-x: hidden" v-model="primaryColor" elevation="15" />
   </v-menu>
