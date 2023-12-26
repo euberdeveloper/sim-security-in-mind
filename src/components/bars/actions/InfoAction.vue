eceb7336c610bf86798e1f87ffcee82077709462
euberdeveloper <euberdeveloper@gmail.com>
2023-12-26 21:04:42 +0100

    feat(bar): fix info dialog
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/components/bars/actions/InfoAction.vue
+++ src/components/bars/actions/InfoAction.vue
@@ -3,13 +3,40 @@ import { storeToRefs } from 'pinia';
 import { useMenuStore } from '@/stores/menu';
 
 const menuStore = useMenuStore();
-const { showInfoButton, showInfo } = storeToRefs(menuStore);
+const { infoText, showInfoButton, showInfo } = storeToRefs(menuStore);
 </script>
 
 <template>
-  <v-tooltip text="Info pagina" location="bottom" v-if="showInfoButton">
-    <template v-slot:activator="{ props }">
-      <v-btn v-bind="props" icon="mdi-information-outline" @click="showInfo = true" />
+  <v-dialog v-model="showInfo" :width="650" scrollable>
+    <template v-slot:activator="{ props: dialogProps }">
+      <v-tooltip text="Info pagina" location="bottom" v-if="showInfoButton">
+        <template v-slot:activator="{ props: tooltipProps }">
+          <v-btn v-bind="{...tooltipProps, ...dialogProps}" icon="mdi-information-outline" />
+        </template>
+      </v-tooltip>
     </template>
-  </v-tooltip>
+
+    <template v-slot:default="{ isActive }">
+      <v-card>
+        <v-toolbar color="primary" dark>
+          <v-toolbar-title class="text-center font-weight-bold">Informazioni</v-toolbar-title>
+        </v-toolbar>
+
+        <v-card-text class="info-text">
+          <slot>
+            <!-- <vue-markdown class="content">{{ infoText }}</vue-markdown> -->
+            <span class="content">{{ infoText }}</span>
+          </slot>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="pa-3">
+          <v-spacer></v-spacer>
+          <v-btn color="success" variant="outlined" @click="isActive.value = false">CHIUDI</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
 </template>
