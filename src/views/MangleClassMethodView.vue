83b957ce3e540b5b1a808e37025498a84ec73501
euberdeveloper <euberdeveloper@gmail.com>
2023-12-26 01:21:18 +0100

    feat: add class method
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/views/MangleClassMethodView.vue
+++ src/views/MangleClassMethodView.vue
@@ -1,5 +1,51 @@
+<script setup lang="ts">
+import { mangleMethodDefinition, mangleMethodSignature, validateMethodSignature } from "lifeware-java-mangler";
+import { storeToRefs } from 'pinia';
+
+import { useClipboard } from '@/compositions/clipboard';
+import { useMangling, useManglingValidation } from '@/compositions/mangler';
+import { useManglersStore } from '@/stores/manglers';
+
+const { copyToClipboard } = useClipboard();
+const { classMethod } = storeToRefs(useManglersStore());
+const { isInputValid, mangledValidationRule } = useManglingValidation(classMethod, validateMethodSignature);
+const { displayedMangled: displayedMangledMethodSignature } = useMangling(classMethod, isInputValid, mangleMethodSignature);
+const { displayedMangled: displayedMangledMethodDefinition } = useMangling(classMethod, isInputValid, mangleMethodDefinition);
+</script>
+
 <template>
-  <div>
-    <h1>Mangle Class method View</h1>
-  </div>
+  <v-card-text class="mt-4 mx-2">
+    <v-text-field
+      v-model="classMethod"
+      type="text"
+      label="Class method"
+      variant="outlined"
+      density="compact"
+      :rules="[mangledValidationRule]"
+    />
+    <v-text-field
+      :value="displayedMangledMethodSignature"
+      type="text"
+      label="Mangled signature"
+      variant="outlined"
+      density="compact"
+      readonly
+      persistent-placeholder
+      append-inner-icon="mdi-content-copy"
+      @click:append-inner="copyToClipboard(displayedMangledMethodSignature)"
+      v-if="displayedMangledMethodSignature"
+    />
+    <v-textarea
+      :value="displayedMangledMethodDefinition"
+      type="text"
+      label="Mangled definition"
+      variant="outlined"
+      density="compact"
+      readonly
+      persistent-placeholder
+      append-inner-icon="mdi-content-copy"
+      @click:append-inner="copyToClipboard(displayedMangledMethodDefinition)"
+      v-if="displayedMangledMethodDefinition"
+    />
+  </v-card-text>
 </template>
