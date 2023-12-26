dd7eb608eebdda5e7cf8547040c423f881ad8407
euberdeveloper <euberdeveloper@gmail.com>
2023-12-26 02:21:47 +0100

    feat: add vue pwa
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest
+    }),
+    vue()
   ],
   define: {
     '__APP_VERSION__': JSON.stringify(process.env.npm_package_version)
