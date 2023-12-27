b366652b7a0b29c1488a1d990e15032023e1a329
euberdeveloper <euberdeveloper@gmail.com>
2023-12-27 22:27:18 +0100

    feat(markdown): add the possibility for plugins
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/directives/VMarkdown.ts
+++ src/directives/VMarkdown.ts
@@ -2,8 +2,38 @@ import type { Directive } from 'vue';
 import markdownit from 'markdown-it';
 import type MarkdownIt from 'markdown-it';
 
-function getVMarkdown(options: MarkdownIt.Options = {}): Directive {
+export interface PluginSimpleItem {
+  type: 'simple',
+  plugin: MarkdownIt.PluginSimple
+}
+export interface PluginWithOptionsItem<T> {
+  type: 'with-options',
+  plugin: MarkdownIt.PluginWithOptions<T>,
+  options: T
+}
+export interface PluginWithParamsItem {
+  type: 'with-params',
+  plugin: MarkdownIt.PluginWithParams,
+  params: any[]
+}
+export type PluginItem = PluginSimpleItem | PluginWithOptionsItem<any> | PluginWithParamsItem;
+
+function getVMarkdown(options: MarkdownIt.Options = {}, plugins: PluginItem[] = []): Directive {
   const md = markdownit(options);
+
+  for (const plugin of plugins) {
+    switch (plugin.type) {
+      case 'simple':
+        md.use(plugin.plugin);
+        break;
+      case 'with-options':
+        md.use(plugin.plugin, plugin.options);
+        break;
+      case 'with-params':
+        md.use(plugin.plugin, ...plugin.params);
+        break;
+    }
+  }
 
   function render(value: string)  {
     return md.render(value);
