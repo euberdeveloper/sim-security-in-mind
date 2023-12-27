5793d69852af36171135d283de7755fab28400da
euberdeveloper <euberdeveloper@gmail.com>
2023-12-27 22:16:39 +0100

    refactor(markdown): use general directive
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/directives/VMarkdown.ts
+++ src/directives/VMarkdown.ts
@@ -1,16 +1,25 @@
 import type { Directive } from 'vue';
 import markdownit from 'markdown-it';
-const md = markdownit();
+import type MarkdownIt from 'markdown-it';
 
-function render(value: string) {
-  return md.render(value);
+function getVMarkdown(options: MarkdownIt.Options = {}): Directive {
+  const md = markdownit(options);
+
+  function render(value: string)  {
+    return md.render(value);
+  }
+
+  return {
+    beforeMount(el, binding) {
+      el.innerHTML = render(binding.value);
+    },
+    beforeUpdate(el, binding) {
+      el.innerHTML = render(binding.value);
+    }
+  };
+
 }
 
-export const vMarkdown: Directive = {
-  beforeMount(el, binding) {
-    el.innerHTML = render(binding.value);
-  },
-  beforeUpdate(el, binding) {
-    el.innerHTML = render(binding.value);
-  }
-};
+export const vMarkdown = getVMarkdown();
+
+export default getVMarkdown;
