f1e35e35911aabdf86c89f1d496efd8b08c4d685
euberdeveloper <euberdeveloper@gmail.com>
2023-12-27 16:20:37 +0100

    feat(preferences): make it responsive
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/views/PreferencesView.vue
+++ src/views/PreferencesView.vue
@@ -20,28 +20,28 @@ const { resetCurrentTheme } = themeStore;
             <v-col :cols="12">
                 <h3>Color palette</h3>
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.primary" type="color" label="Primary" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.secondary" type="color" label="Secondary" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.success" type="color" label="Success" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.info" type="color" label="Info" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.warning" type="color" label="Warning" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.error" type="color" label="Error" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.background" type="color" label="Background" />
             </v-col>
-            <v-col :cols="3">
+            <v-col :md="3" :cols="6">
                 <v-text-field v-model="themeColors.surface" type="color" label="Surface" />
             </v-col>
             <v-col>
