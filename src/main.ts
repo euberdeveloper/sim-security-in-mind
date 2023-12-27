fc431bb51c0545b45835e4952f81e4e4cc789442
euberdeveloper <euberdeveloper@gmail.com>
2023-12-27 17:49:56 +0100

    feat: add vue-gtag
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+
+import VueGtag from "vue-gtag";
+
 import vuetify from '@/plugins/vuetify';
 import { vMarkdown } from '@/directives/VMarkdown';
 
@@ -11,6 +14,9 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueGtag, {
+    config: { id: "G-B5PQ10HS0B" }
+}, router);
 app.use(vuetify);
 app.directive('markdown', vMarkdown);
 
