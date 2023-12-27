95e99143e4f6861dcd42ac9befe51cbeaa3a28bd
euberdeveloper <euberdeveloper@gmail.com>
2023-12-27 17:38:33 +0100

    feat: roolback without gtag manager
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-
-import { createGtm } from '@gtm-support/vue-gtm';
-
 import vuetify from '@/plugins/vuetify';
 import { vMarkdown } from '@/directives/VMarkdown';
 
@@ -14,12 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(
-    createGtm({
-      id: 'GTM-KGMJMB6W',
-      vueRouter: router
-    }),
-  );
 app.use(vuetify);
 app.directive('markdown', vMarkdown);
 
