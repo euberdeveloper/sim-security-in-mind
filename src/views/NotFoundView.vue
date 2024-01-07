21134282ece65b1163ffe8cf0f925e899db661e0
euberdeveloper <euberdeveloper@gmail.com>
2024-01-07 23:28:01 +0100

    style: format
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row>
       <v-col :cols="12">
-        <h2 class="my-4 text-center">{{ $t('views.not-found.text', {path:$route.path}) }}</h2>
+        <h2 class="my-4 text-center">{{ $t('views.not-found.text', { path: $route.path }) }}</h2>
         <img class="my-4 image" src="@/assets/not_found.png" alt="not found" />
       </v-col>
       <v-col class="d-flex justify-center">
